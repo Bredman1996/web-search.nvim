12b301a00fb0dea222ebf0fdc647a24f880ea772
Brandon Redman <bredman1996@gmail.com>
2026-01-13 21:59:01 -0600

    maybe learn how to type moron

--- lua/web-search/commands.lua
+++ lua/web-search/commands.lua
@@ -8,6 +8,6 @@ vim.api.nvim_create_user_command("WebSearchSelection", function()
 	core.search_highlighted()
 end, {})
 
-vim.api.nvim.nvim_create_user_command("WebSearchTerraform", function()
+vim.api.nvim_create_user_command("WebSearchTerraform", function()
 	core.search_tf()
 end, {})
